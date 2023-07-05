8950340166caf84ff391fa8eb00170ae0b5b0e9b
Amal Wijewardane <amal.wijewardane@exactprosystems.com>
2023-07-05 10:40:17 +0530

    adding HrmAdminTests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,17 @@ import { PlaywrightTestConfig, defineConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig ={
 
+  //testMatch:["tests\hrmadminPage.test.ts"],
     use:{
       
-        headless:false
+        headless:true
+        
       },
       reporter: [["dot"],["json",{
         outputFile:"jsonReports/Jreport.json"
-      }],["html",{open:"always"}]] 
-  
+      }],["html",{open:"always"}]] ,
+      timeout: 30000
+     
+      
 }
 export default config;
---
+++ src/pages/hrmAdminPage.ts
@@ -0,0 +1,86 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class hrmAdminPage {
+    readonly page: Page;
+
+    //Page elements
+    private userName = "//*[@id='app']/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input"
+    private empName = "//*[@id='app']/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div/input"
+    private searchBtn = "//*[@id='app']/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]"
+    private firstname = "//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div"
+    private userRole = "//*[@id='app']/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/div[1]"
+    private jobdd;
+
+    //////////////
+
+    private pickUser;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    private async enteruserName() {
+        await this.page.waitForSelector(this.userName);
+        await this.page.fill(this.userName, "Admin");
+        console.log("Username filled");
+    }
+    private async enterempName() {
+        await this.page.waitForSelector(this.empName);
+        await this.page.fill(this.empName, "");
+        console.log("empname filled");
+    }
+    private async enterUserRole() {
+        await this.page.locator('form i').first().click();
+        await this.page.getByRole('option', { name: 'Admin' }).click();
+        console.log("UserRole filled");
+    }
+    private async enterStatus() {
+        await this.page.locator('form i').nth(1).click();
+        await this.page.getByRole('option', { name: 'Enabled' }).getByText('Enabled').click();
+        console.log("Status filled");
+    }
+    private async clickSearchButton() {
+        await Promise.all([
+            await expect(this.page.locator(this.searchBtn)).toBeVisible(),
+            await this.page.click(this.searchBtn)
+        ]);
+        console.log("Search Button clicked")
+    }
+
+    private async clickjobtitles(){
+        await this.page.getByText('Job').click();
+        await this.page.getByRole('menuitem',{name : 'Job Titles'}).click();
+        console.log("JobTitle picked");
+
+    }
+
+     async searchUser(){
+           await this.enteruserName();
+            await this.enterempName();
+            await this.enterUserRole();
+            await this.enterStatus();
+            await this.clickSearchButton();
+        }
+
+        async gotoajobtittle(){
+           
+             await this.clickjobtitles();
+         }
+
+    private async validateSearchFeature() {
+        const value1 = this.page.locator(this.firstname);
+        await expect(value1).toHaveText('Admin', { timeout: 70000 })
+    }
+    async verifySearchFeature() {
+        await this.validateSearchFeature();
+    }
+    private async validateJobTitle() {
+        const value2 = this.page.getByText('Job Titles');
+        await expect(value2).toHaveText('Job Titles', { timeout: 70000 })
+    }
+    async verifyJobTitle() {
+        await this.validateJobTitle();
+    }
+
+   
+}
